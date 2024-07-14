7710627e5ab643f1fe23bfdaa83ed95abd26021b
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-07-14 13:31:10 -0400

    Player controller is added
    I added my controller to the game, ill have to adjust it alot but figured its better to work on something that I know/ how it works

---
+++ BrianAndJaysHorrorProject/Assets/Code/PlayerCam.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform PlayerBody;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        //Changed Time.deltaTime to fixed to fix jidder of camera.
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.fixedDeltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.fixedDeltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  
+
+        //rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
--- BrianAndJaysHorrorProject/Assets/Code/PlayerController.cs
+++ BrianAndJaysHorrorProject/Assets/Code/PlayerController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController Controller;
 
-    // Update is called once per frame
+    public float Speed = 12f;
+    public float Gravity = -9.81f;
+    public float JumpHeight = 3f;
+    public float Runspeed = 16;
+
+
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+
+
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        Controller.Move(move * Speed * Time.deltaTime);
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Controller.Move(move * Runspeed * Time.deltaTime);
+        }
         
+        velocity.y += Gravity * Time.deltaTime;
+
+        Controller.Move(velocity * Time.deltaTime);
     }
 }
