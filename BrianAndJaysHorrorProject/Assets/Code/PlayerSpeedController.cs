9152a5d77b42035f9dee9378937629cabb2d680e
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-07-20 21:45:15 -0400

    Made the surface slow down function
    I can't think of another way of doing this just now, but it works and will refactor it later if it messes with performance.

---
+++ BrianAndJaysHorrorProject/Assets/Code/PlayerSpeedController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpeedController : MonoBehaviour
+{
+    [Header("Speeds")]
+    public int NormalSpeed;
+    public int SlowedSpeed;
+    //Add more here..
+
+    public void OnTriggerEnter(Collider other)
+    {
+        CheckGroundTagAndSet(other);
+    }
+    /// <summary>
+    /// Check when the player hits the ground, what layer the player is running on, then changes the speed of the player.
+    /// </summary>
+    /// <param name="groundCollider"></param>
+    private void CheckGroundTagAndSet(Collider groundCollider)
+    {
+        if (groundCollider.gameObject.CompareTag("Ground"))
+            GetComponent<PlayerController>().Speed = NormalSpeed;
+        else if (groundCollider.gameObject.CompareTag("SlowGround"))
+            GetComponent<PlayerController>().Speed = SlowedSpeed;
+        //^Trying this out, seeing if it will impact performance, if does, just going to ref the script instead of get component.
+    }
+}
