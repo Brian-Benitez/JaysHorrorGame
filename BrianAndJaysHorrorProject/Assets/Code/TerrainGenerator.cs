29828b46b82e5b182173d7d0b0a3904562e998da
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-08-02 19:49:42 -0400

    REFACTORED + ADDED NEW FUNCTIONS
    I cleaned up some bits of code and then i added a function to change the child's tags of the terrain objects to a random picked name to make it so when a new terrain spawns in,  they can all be different textures and can add some difficulty to the game when player.

--- BrianAndJaysHorrorProject/Assets/Code/TerrainGenerator.cs
+++ BrianAndJaysHorrorProject/Assets/Code/TerrainGenerator.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainGenerator : MonoBehaviour
+public class TerrainGenerator : MonoBehaviour//I ALREADY NEED TO CHANGE THE NAME OF THIS CLASS....
 {
     [Header("Transforms")]
     public List<GameObject> Terrains;
+    public int AmountOfChildsInTerrains = 3;
 
-    public float SpaceZ = 2.0f;
-    public bool IsPressed = false;
+    [Header("Space between terrains")]
+    [SerializeField]
+    private float SpaceZ = 2.0f;
+
+    [Header("Booleans")]
+    [SerializeField]
+    private bool IsPressed = false;
+
+    [Header("Strings for tags")]
+    [SerializeField]
+    public List<string> TerrainTagNames = new List<string>();
 
     private void Update()
     {
@@ -16,12 +26,32 @@ public class TerrainGenerator : MonoBehaviour
             GenerateNewTerrain();
         IsPressed = false;
     }
+    private void SetCloneToMainObject()
+    {
 
-    public void GenerateNewTerrain()
+    }
+
+    private void GenerateNewTerrain()
     {
         IsPressed = true;   
-        GameObject SecondTerrain = Instantiate<GameObject>(Terrains[0]);
+        GameObject SecondTerrain = Instantiate<GameObject>(Terrains[1]);
         SecondTerrain.transform.position = Terrains[0].transform.position + Terrains[0].transform.forward * SpaceZ;
+        ChangeTagsOnChildTerrains(SecondTerrain);
         Debug.Log("spawn new terrain");
+    }
+
+    private void ChangeTagsOnChildTerrains(GameObject NewTerrain)
+    {
+        int index = Random.Range(0, TerrainTagNames.Count);
+        NewTerrain.transform.gameObject.tag = TerrainTagNames[index];
+        //Then change the child of the main objects tags...
+        for (int i = 0; i < AmountOfChildsInTerrains - 1; i++)
+        {
+            int newIndex = Random.Range(0, TerrainTagNames.Count);
+            Debug.Log("new tag name " + TerrainTagNames[newIndex]);
+            NewTerrain.transform.GetChild(i).gameObject.tag = TerrainTagNames[newIndex];
+            Debug.Log("name " + Terrains[1].transform.GetChild(i).name);
+            Debug.Log("Child num " + i);
+        }
     }
 }
