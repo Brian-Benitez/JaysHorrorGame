df456080888525dd6ebf19feefb7f6bf84fa2e27
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-09-01 20:16:06 -0400

    Work In Progress: Added new functions and checks
    Still need to test terrainChunks the function before I can say it works.

--- BrianAndJaysHorrorProject/Assets/Code/TerrainGenerator.cs
+++ BrianAndJaysHorrorProject/Assets/Code/TerrainGenerator.cs
@@ -9,26 +9,43 @@ public class TerrainGenerator : MonoBehaviour//I ALREADY NEED TO CHANGE THE NAME
 
     [Header("Numbers")]
     public int AmountOfChildsInTerrains = 3;
-
-    [Header("Space between terrains")]
-    [SerializeField]
-    private float SpaceZ = 2.0f;
+    public int MaxAmountOfChunks;
 
     [Header("Strings for tags")]
     [SerializeField]
     public List<string> TerrainTagNames = new List<string>();
 
     /// <summary>
-    /// 
     /// Instantiate a new terrain chunk, add it to the list of terrains, then move it to posistion. 
     /// </summary>
     public void GenerateNewTerrain(Transform SpawnTerrainPoint)
     {
-        GameObject SecondTerrain = Instantiate<GameObject>(TerrainChunks[0]);
-        TerrainChunks.Add(SecondTerrain);// need to also delete it later when im not on the platform.
-        TerrainChunks[1].transform.position = SpawnTerrainPoint.transform.position;// need to make this go through the list of chunks
-        ChangeTagsOnChildTerrains(SecondTerrain);
-        Debug.Log("spawn new terrain");
+        for (int i = 0; i < TerrainChunks.Count; i++)
+        {
+            GameObject NewTerrain = Instantiate<GameObject>(TerrainChunks[0]);
+            TerrainChunks.Add(NewTerrain);// need to also delete it later when im not on the platform.
+            TerrainChunks[i].transform.position = SpawnTerrainPoint.transform.position;// need to make this go through the list of chunks
+            ChangeTagsOnChildTerrains(NewTerrain);
+            Debug.Log("spawn new terrain");
+            return;//check if this works
+        }
+    }
+
+   /// <summary>
+   /// Deletes old chunks that the player already pass through...
+   /// </summary>
+    private void DeleteOldChunks()// Look into this again and see how it works..
+    {
+        if(TerrainChunks.Count > MaxAmountOfChunks)
+        {
+            for(int i = 0; i < MaxAmountOfChunks; i++)
+            {
+                Debug.Log("Deleting chunks of terrain");
+                TerrainChunks[i].gameObject.SetActive(false);
+                TerrainChunks.Remove(TerrainChunks[i]);
+                Destroy(TerrainChunks[i]);//this is kinda expensive but we will see...
+            }
+        }
     }
     /// <summary>
     /// Change the tags of the childs of the terrain so the player can have a diffrent buff or debuff when climbing.
