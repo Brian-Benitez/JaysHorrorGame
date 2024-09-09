1e711f222e785f8b3d5d6a9a36f9435398a37f44
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-09-08 22:28:46 -0400

    Refactored: Change terrain texture
    The first check i made was not right, needed to check the tags of the child's not  the angle. It shows some pink textures but I think its because I need the texture which I dont have yet.

--- BrianAndJaysHorrorProject/Assets/Code/ChangeTerrainTextures.cs
+++ BrianAndJaysHorrorProject/Assets/Code/ChangeTerrainTextures.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ChangeTerrainTextures : MonoBehaviour
 {
     [Header("Textures")]
     public Material SlowTexture, FastTexture, NormalTexture;
+    [Header("Amount of childs in chunk")]
+    [Header("Make sure if you change how much childs are in a chunk to change it here!")]
+    public int AmountOfChilds = 2;
     
-
     [Header("Scripts")]
     public TerrainGenerator TerrainGeneratorRef;
     public PlayerSpeedController PlayerSpeedControllerRef;
@@ -16,11 +19,13 @@ public class ChangeTerrainTextures : MonoBehaviour
     {
         Renderer[] v = chunk.GetComponentsInChildren<Renderer>();
 
-        foreach (Renderer r in v)
+        for (int i = 0; i < AmountOfChilds; i++)
         {
-            if (chunk.transform.rotation.eulerAngles.x == PlayerSpeedControllerRef.FlatSlope)
-                r.sharedMaterial = NormalTexture;
-            //Add more here.
+            if(chunk.transform.GetChild(i).CompareTag("Ground"))
+                v[i].sharedMaterial = NormalTexture;
+            else if(chunk.transform.GetChild(i).CompareTag("SlowGround"))
+                v[i].sharedMaterial = SlowTexture;
+            //Add more here...
         }
     }
 }
