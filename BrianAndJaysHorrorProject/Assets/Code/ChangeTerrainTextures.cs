40094eb5ccbce93026d9c1afc51a65a6071f3d21
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-09-09 20:58:29 -0400

    Fixed: Materials not going to the right tags
    I know this looks bad, but for now I just wanted to see what was wrong and now I can fix it.

--- BrianAndJaysHorrorProject/Assets/Code/ChangeTerrainTextures.cs
+++ BrianAndJaysHorrorProject/Assets/Code/ChangeTerrainTextures.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Globalization;
 using System.Linq;
 using UnityEngine;
 
@@ -18,14 +20,31 @@ public class ChangeTerrainTextures : MonoBehaviour
     public void ChangeTexturesOfChunk(GameObject chunk)
     {
         Renderer[] v = chunk.GetComponentsInChildren<Renderer>();
+        Renderer r = chunk.GetComponent<Renderer>();
+
+        if (chunk.transform.CompareTag("Ground"))
+            r.material = NormalTexture;
+        else if (chunk.transform.CompareTag("SlowGround"))
+            r.material = SlowTexture;
+
 
         for (int i = 0; i < AmountOfChilds; i++)
         {
-            if(chunk.transform.GetChild(i).CompareTag("Ground"))
-                v[i].sharedMaterial = NormalTexture;
-            else if(chunk.transform.GetChild(i).CompareTag("SlowGround"))
-                v[i].sharedMaterial = SlowTexture;
-            //Add more here...
+            if (chunk.transform.GetChild(i).CompareTag("Ground"))
+            {
+                Debug.Log("this needs ground texture");
+                Debug.Log("Name " + chunk.transform.GetChild(i).name);
+                v[i+1].material = NormalTexture;//added a +1 to the i index because the renderer also takes the parents renderer
+            }
+                
+            else if (chunk.transform.GetChild(i).CompareTag("SlowGround"))
+            {
+                Debug.Log("use slow ground");
+                Debug.Log("Name " + chunk.transform.GetChild(i).name);
+                v[i+1].material = SlowTexture;
+            }
+              
+            Debug.Log("how many runs " +  i);   
         }
     }
 }
